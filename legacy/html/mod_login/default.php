86a874e559fca31455e4017ae63ed0930dacccf6
Benn <nocniagenti@gmail.com>
2014-09-17 21:30:45 +0200

    mod_login match j3.x layout

--- legacy/html/mod_login/default.php
+++ legacy/html/mod_login/default.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 ?>
 <?php if ($type == 'logout') : ?>
-<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" class="form-form">
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" class="yjsg-form">
 <?php if ($params->get('greeting')) : ?>
 	<div class="login-greeting">
 	<?php if($params->get('name') == 0) : {
@@ -22,8 +22,8 @@ JHtml::_('behavior.keepalive');
 	} endif; ?>
 	</div>
 <?php endif; ?>
-	<div class="logout-button">
-        <button type="submit" tabindex="3" name="Submit" class="btn btn-small btn-sm button"><?php echo JText::_('JLOGOUT') ?></button>
+	<div class="yjsg-form-group">
+        <button type="submit" tabindex="3" name="Submit" class="button"><?php echo JText::_('JLOGOUT') ?></button>
 		<input type="hidden" name="option" value="com_users" />
 		<input type="hidden" name="task" value="user.logout" />
 		<input type="hidden" name="return" value="<?php echo $return; ?>" />
@@ -31,32 +31,33 @@ JHtml::_('behavior.keepalive');
 	</div>
 </form>
 <?php else : ?>
-<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" class="form-form yjsg-form">
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" class="yjsg-form">
 	<div class="pretext">
 	<?php echo $params->get('pretext'); ?>
 	</div>
-	<div class="input-prepend input-group">
-    	<span class="add-on input-group-addon"><span class="icon-user"></span></span>
-		<input id="modlgn_username" type="text" name="username" class="inputbox form-control form-field"  size="18" placeholder="<?php echo JText::_('MOD_LOGIN_VALUE_USERNAME') ?>" />
-    </div>
-    <div class="input-prepend input-group">
-    	<span class="add-on input-group-addon"><span class="icon-lock"></span></span>
-		<input id="modlgn_passwd" type="password" name="password" class="inputbox form-control form-field" size="18" placeholder="<?php echo JText::_('JGLOBAL_PASSWORD') ?>" />
-    </div>
-        
-        
-        
+		<div class="yjsg-form-group-addon">
+    		<span class="yjsg-form-prepend"><span class="icon-user"></span></span>
+			<input id="modlgn_username" type="text" name="username" class="yjsg-form-element"  size="18" placeholder="<?php echo JText::_('MOD_LOGIN_VALUE_USERNAME') ?>" />
+		</div>
+		<div class="yjsg-form-group-addon">
+    		<span class="yjsg-form-prepend"><span class="icon-lock"></span></span>
+			<input id="modlgn_passwd" type="password" name="password" class="yjsg-form-element" size="18" placeholder="<?php echo JText::_('JGLOBAL_PASSWORD') ?>" />
+		</div>
+    <div class="yjsg-form-group-inline">    
 	<?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
-       <label class="checkbox" id="modlgn_remember_l">
-        <input id="modlgn_remember" type="checkbox"><?php echo JText::_('MOD_LOGIN_REMEMBER_ME') ?>
+      <label class="checkbox-label" id="modlgn_remember_l">
+        	<input id="modlgn_remember" type="checkbox"> <?php echo JText::_('MOD_LOGIN_REMEMBER_ME') ?>
       </label>
 	<?php endif; ?>
-    <button type="submit" tabindex="3" name="Submit" class="btn btn-small btn-sm button"><?php echo JText::_('JLOGIN') ?></button>
+	<div class="yjsg-element-holder">
+   	 	<button type="submit" tabindex="3" name="Submit" class="button"><?php echo JText::_('JLOGIN') ?></button>
+	</div>
+	</div>
 	<input type="hidden" name="option" value="com_users" />
 	<input type="hidden" name="task" value="user.login" />
 	<input type="hidden" name="return" value="<?php echo $return; ?>" />
 	<?php echo JHtml::_('form.token'); ?>
-	<ul>
+	<ul class="unstyled">
 		<li>
 			<a href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>">
 			<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
