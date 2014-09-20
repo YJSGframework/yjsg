c00d6d0b073564e3ec0b7d2de99767001b6b6370
stefan <nocniagenti@gmail.com>
2014-09-20 22:15:33 +0200

    backup pre v2 template on install

--- install.script.php
+++ install.script.php
@@ -8,37 +8,91 @@
  */
 // no direct access 
 defined('_JEXEC') or die();
-class plgSystemYJSGInstallerScript{
+class plgSystemYJSGInstallerScript {
+    
+    
+    
+    /**
+     * Yjsg instance
+     *
+     * @return	Yjsg
+     * @since 2.0.0
+     */
+    
+    
+    public $yjsg;
+    
+	/**
+	 * method to run after an install/update/uninstall method
+	 *
+	 * @return void
+	 */    
+    
+    public function postflight($type, $parent) {
+        
+        
+        
+        $db = JFactory::getDbo();
+        
+        try {
+            
+            $q = $db->getQuery(true);
+            
+            $q->update('#__extensions');
+            $q->set(array(
+                'enabled = 1',
+                'ordering = -5000'
+            ));
+            $q->where("element = 'yjsg'");
+            $q->where("type = 'plugin'", 'AND');
+            $q->where("folder = 'system'", 'AND');
+            
+            $db->setQuery($q);
+            
+            method_exists($db, 'execute') ? $db->execute() : $db->query();
+        }
+        catch (Exception $e) {
+            throw $e;
+        }
+        
+        JLoader::register('Yjsg', JPATH_ROOT . '/plugins/system/yjsg/includes/yjsgcore/classes/yjsg.class.php', true);
+        
+        $this->yjsg = Yjsg::getInstance();
+        
+        
+        $language 				= JFactory::getLanguage();
+        $language->load('plg_system_yjsg', JPATH_ADMINISTRATOR);
+		$yjsgTemplateName 		= Yjsg::getDefaultTemplate();
+        $yjsgTemplateNameTxt 	= ucfirst(Yjsg::getDefaultTemplate());
+        $defaultTemplate 		= JPATH_ROOT . '/templates/' . $yjsgTemplateName;
+        $beforeCleanup   		= JPATH_ROOT . '/templates/' . $yjsgTemplateName . '-beforeCleanup';
+		$extendFolder			= JPATH_ROOT . '/plugins/system/yjsg/includes/yjsgcore/classes/extend/classes/'; 
+		$indexContent 			= '';      
+        
+        if (!JFolder::exists($beforeCleanup) && $this->yjsg->yjtmpl() && $this->yjsg->tmplVersion($yjsgTemplateName) == '1.0.16') {
 
-		public function postflight( $type, $parent )  {
-			  $db = JFactory::getDbo();
-		
-			  try
-			  {
-
-				 $q = $db->getQuery(true);
-		
-				 $q->update('#__extensions');
-				 $q->set(array('enabled = 1','ordering = -5000'));
-				 $q->where("element = 'yjsg'");
-				 $q->where("type = 'plugin'", 'AND');
-				 $q->where("folder = 'system'", 'AND');
-		
-				 $db->setQuery($q);
-		
-				 method_exists($db, 'execute') ? $db->execute() : $db->query();
-			  }
-			  catch (Exception $e)
-			  {
-				 throw $e;
-			  }
-			  
-
-			if(!JFolder::exists(JPATH_SITE . '/plugins/system/yjsg/includes/yjsgcore/classes/extend/classes/')) return;
 			
-            JFolder::delete(JPATH_SITE . '/plugins/system/yjsg/includes/yjsgcore/classes/extend/classes/');
-            JFolder::create(JPATH_SITE . '/plugins/system/yjsg/includes/yjsgcore/classes/extend/classes/');
-            $indexContent = '';
-            JFile::write(JPATH_SITE . '/plugins/system/yjsg/includes/yjsgcore/classes/extend/classes/index.html', $indexContent);			  
+			if (JFolder::copy($defaultTemplate, $beforeCleanup)) {
+
+			    $backedUpMsg = $yjsgTemplateNameTxt . JText::_('YJSG_INSTALLER_TMPL_BACKUP');
+                JFactory::getApplication()->enqueueMessage($backedUpMsg);
+
+			} else {
+
+                $nobackupMsg = JText::_('YJSG_INSTALLER_TMPL_NO_BACKUP1') .$yjsgTemplateNameTxt  . JText::_('YJSG_INSTALLER_TMPL_NO_BACKUP2');
+                JError::raiseWarning(100, $nobackupMsg);
+
+			}
+			
+
+        }
+        
+        
+        if (JFolder::exists($extendFolder)){
+        
+        	JFolder::delete($extendFolder);
+        	JFolder::create($extendFolder);
+        	JFile::write($extendFolder . 'index.html', $indexContent);
 		}
+    }
 }
