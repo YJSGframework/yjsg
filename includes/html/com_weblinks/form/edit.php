ad32154f204f46a6be71859454d97414d3edda6c
stefan <nocniagenti@gmail.com>
2014-09-23 20:58:25 +0200

    weblinks missing reset form

--- includes/html/com_weblinks/form/edit.php
+++ includes/html/com_weblinks/form/edit.php
@@ -13,12 +13,11 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 Yjsg::yjsgtooltip();
 JHtml::_('behavior.formvalidation');
-$this->form->reset( true ); // to reset the form xml loaded by the view
-$this->form->loadFile( dirname(__FILE__) . YJDS . "edit.xml"); // to load in our own version of login.xml
+
+$this->form->loadFile( dirname(__FILE__) . YJDS . "weblink.xml");
 // Create shortcut to parameters.
 $params = $this->state->get('params');
 ?>
-
 <script type="text/javascript">
 	Joomla.submitbutton = function(task) {
 		if (task == 'weblink.cancel' || document.formvalidator.isValid(document.id('adminForm'))) {
@@ -30,58 +29,69 @@ $params = $this->state->get('params');
 		}
 	}
 </script>
+
 <div class="edit<?php echo $this->pageclass_sfx; ?>">
-<?php if ($this->params->def('show_page_heading', 1)) : ?>
-<h1>
-	<?php echo $this->escape($this->params->get('page_heading')); ?>
-</h1>
-<?php endif; ?>
-<form action="<?php echo JRoute::_('index.php?option=com_weblinks&view=form&w_id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="yjsg-form form-validate">
-	<fieldset class="form-fieldset">
-		<legend><?php echo JText::_('COM_WEBLINKS_LINK'); ?></legend>
-
-			<div class="formelm">
-			<?php echo $this->form->getLabel('title'); ?>
-			<?php echo $this->form->getInput('title'); ?>
+	<?php if ($this->params->def('show_page_heading', 1)) : ?>
+	<h1>
+		<?php echo $this->escape($this->params->get('page_heading')); ?>
+	</h1>
+	<?php endif; ?>
+	<form action="<?php echo JRoute::_('index.php?option=com_weblinks&view=form&w_id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="yjsg-form form-validate yjsg-edit-form">
+		<fieldset class="form-fieldset">
+			<legend><?php echo JText::_('COM_WEBLINKS_LINK'); ?></legend>
+			<div class="yjsg-form-group-inline">
+				<button type="button" class="yjsg-button-blue" onclick="Joomla.submitbutton('weblink.save')">
+				<i class="fa fa-check"></i>
+				<?php echo JText::_('JSAVE') ?>
+				</button>
+				<button type="button" class="yjsg-button" onclick="Joomla.submitbutton('weblink.cancel')">
+				<i class="fa fa-close"></i>
+				<?php echo JText::_('JCANCEL') ?>
+				</button>
 			</div>
-
-			<div class="formelm">
-			<?php echo $this->form->getLabel('alias'); ?>
-			<?php echo $this->form->getInput('alias'); ?>
+			<div class="yjsg-form-group-inline">
+				<?php echo $this->form->getLabel('title'); ?>
+				<div class="yjsg-element-holder">
+					<?php echo $this->form->getInput('title'); ?>
+				</div>
 			</div>
-
-			<div class="formelm">
-			<?php echo $this->form->getLabel('catid'); ?>
-			<?php echo $this->form->getInput('catid'); ?>
+			<div class="yjsg-form-group-inline">
+				<?php echo $this->form->getLabel('alias'); ?>
+				<div class="yjsg-element-holder">
+					<?php echo $this->form->getInput('alias'); ?>
+				</div>
 			</div>
-			<div class="formelm">
-			<?php echo $this->form->getLabel('url'); ?>
-			<?php echo $this->form->getInput('url'); ?>
+			<div class="yjsg-form-group-inline">
+				<?php echo $this->form->getLabel('catid'); ?>
+				<div class="yjsg-element-holder">
+					<?php echo $this->form->getInput('catid'); ?>
+				</div>
+			</div>
+			<div class="yjsg-form-group-inline">
+				<?php echo $this->form->getLabel('url'); ?>
+				<div class="yjsg-element-holder">
+					<?php echo $this->form->getInput('url'); ?>
+				</div>
 			</div>
 			<?php if ($this->user->authorise('core.edit.state', 'com_weblinks.weblink')): ?>
-				<div class="formelm">
+			<div class="yjsg-form-group-inline">
 				<?php echo $this->form->getLabel('state'); ?>
-				<?php echo $this->form->getInput('state'); ?>
+				<div class="yjsg-element-holder">
+					<?php echo $this->form->getInput('state'); ?>
 				</div>
-			<?php endif; ?>
-			<div class="formelm">
-			<?php echo $this->form->getLabel('language'); ?>
-			<?php echo $this->form->getInput('language'); ?>
 			</div>
-			<div class="formelm-buttons">
-			<button type="button" class="button" onclick="Joomla.submitbutton('weblink.save')">
-				<?php echo JText::_('JSAVE') ?>
-			</button>
-			<button type="button" class="button" onclick="Joomla.submitbutton('weblink.cancel')">
-				<?php echo JText::_('JCANCEL') ?>
-			</button>
+			<?php endif; ?>
+			<div class="yjsg-form-group-inline">
+				<?php echo $this->form->getLabel('language'); ?>
+				<div class="yjsg-element-holder">
+					<?php echo $this->form->getInput('language'); ?>
+				</div>
 			</div>
 			<div>
-			<?php echo $this->form->getLabel('description'); ?>
-			<?php echo $this->form->getInput('description'); ?>
+				<?php echo $this->form->getLabel('description'); ?>
+				<?php echo $this->form->getInput('description'); ?>
 			</div>
-	</fieldset>
-
+		</fieldset>
 		<input type="hidden" name="return" value="<?php echo $this->return_page;?>" />
 		<input type="hidden" name="task" value="" />
 		<?php echo JHtml::_( 'form.token' ); ?>
