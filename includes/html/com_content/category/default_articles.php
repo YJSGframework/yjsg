1c901721780b5cc4530ffd28ebd7b89b9790da83
stefan <nocniagenti@gmail.com>
2015-01-29 15:56:36 +0100

    submit new article assign cat fixes #13

--- includes/html/com_content/category/default_articles.php
+++ includes/html/com_content/category/default_articles.php
@@ -157,7 +157,7 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 
 <?php // Code to add a link to submit an article. ?>
 <?php if ($this->category->getParams()->get('access-create')) : ?>
-	<?php echo JHtml::_('icon.create', $article, $article->params); ?>
+	<?php echo JHtml::_('icon.create', $this->category, $this->category->params); ?>
 <?php  endif; ?>
 	<?php if (($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
 	<div class="yjsg-pagination">
