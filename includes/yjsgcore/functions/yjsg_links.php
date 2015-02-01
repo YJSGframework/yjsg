e5ae99c70047b024b60ff3955e5e318fba1115ed
OpenWeb <webmail@openweb.pt>
2015-02-01 17:04:44 +0000

    Final changes (links)
    now tests are needed

--- includes/yjsgcore/functions/yjsg_links.php
+++ includes/yjsgcore/functions/yjsg_links.php
@@ -18,28 +18,29 @@ function getYJLINKS($default_font_family,$yj_copyrightear,$yj_templatename,$show
 	if((function_exists('toolbox_urls') && $show_tools == 1) || $validators_off == 1 || $totop_off == 1){
 		echo '<div class="validators">';
 		if($validators_off == 1){
-			$allLinks[] ='<a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" target="_blank" title="CSS Validity">CSS Valid</a>';
-			$allLinks[] ='<a href="http://validator.w3.org/check/referer" target="_blank" title="XHTML Validity">XHTML Valid</a>';
+			$allLinks[] ='<a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" target="_blank" title="'.JText::_('YJSG_LINKS_VALIDCSS_TITLE').'">'.JText::_('YJSG_LINKS_VALIDCSS').'</a>';
+			$allLinks[] ='<a href="http://validator.w3.org/check/referer" target="_blank" title="'.JText::_('YJSG_LINKS_VALIDHTML_TITLE').'">'.JText::_('YJSG_LINKS_VALIDHTML').'</a>';
 		}
 		if($totop_off == 1){
-			$allLinks[] ='<a class="yjscroll" href="#stylef'.$default_font_family.'">Top</a>';
+			$allLinks[] ='<a class="yjscroll" href="#stylef'.$default_font_family.'">'.JText::_('YJSG_LINKS_TOP').'</a>';
 		}
 		if (function_exists('toolbox_urls') && $show_tools == 1):
 		global $font_size;
 		global $font_direction;
 			if ($show_fres == 1):
-				$allLinks[] = '<a id="fontSizePlus" class="fs" href="javascript:;" rel="nofollow">+</a>';
-				$allLinks[] = '<a id="fontSizeMinus"  class="fs" href="javascript:;" rel="nofollow">-</a>';
-				$allLinks[] = '<a id="fontSizeReset"  class="fs" href="javascript:;" rel="nofollow">reset</a>';
+				$allLinks[] = '<a id="fontSizePlus" class="fs" href="javascript:;" rel="nofollow">'.JText::_('YJSG_LINKS_BIGGERTEXT').'</a>';
+				$allLinks[] = '<a id="fontSizeMinus"  class="fs" href="javascript:;" rel="nofollow">'.JText::_('YJSG_LINKS_SMALLERTEXT').'</a>';
+				$allLinks[] = '<a id="fontSizeReset"  class="fs" href="javascript:;" rel="nofollow">'.JText::_('YJSG_LINKS_RESET').'</a>';
 			endif;
 			if ($show_rtlc == 1):
-				$allLinks[] = '<a class="tdir" href="'.$font_direction[1].'" rel="nofollow">RTL</a>';
-				$allLinks[] = '<a class="tdir" href="'.$font_direction[2].'" rel="nofollow">LTR</a>';
+				$allLinks[] = '<a class="tdir" href="'.$font_direction[1].'" rel="nofollow">'.JText::_('YJSG_LINKS_RTL').'</a>';
+				$allLinks[] = '<a class="tdir" href="'.$font_direction[2].'" rel="nofollow">'.JText::_('YJSG_LINKS_LTR').'</a>';
 			endif;
 		endif;
 		echo implode(' | ',$allLinks);
 		echo '</div>';
 	}
-	echo'<div class="yjsgcp">Copyright &copy; <span>'.$yj_templatename.'</span> '.$yj_copyrightear.' All rights reserved. <a href="http://www.youjoomla.com" title="Joomla Templates Club">Custom Design by Youjoomla.com </a></div>';
+	
+	echo '<div class="yjsgcp">'.JText::sprintf('YJSG_LINKS_YOUJOOMLA_CREDITS', $yj_templatename, $yj_copyrightear).'</div>';
 }
 ?>
