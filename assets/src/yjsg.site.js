ad648acfedc6fc33866662dac17d10aea2a63ab7
danyj <nocniagenti@gmail.com>
2019-02-20 22:57:02 +0100

    CHrome Vimeo and Youtube autoplay fix

--- assets/src/yjsg.site.js
+++ assets/src/yjsg.site.js
@@ -265,11 +265,96 @@
 			
 		   $('.yjsg-iframe-lightbox').magnificPopup({
 			  type: 'iframe',
-			  mainClass: 'mfp-fade'
+			  mainClass: 'mfp-fade',
+			  iframe: self.yjsgMagnificIframeExtend(),
 			});
 			
 
         },
+		
+		yjsgMagnificIframeExtend: function ($counter){
+			
+			var self 	= this;
+			var $start 	= 0;
+			
+			var $markup ='<div class="mfp-iframe-scaler">';
+				$markup +='<div class="mfp-close"></div>';
+				$markup +='<iframe class="mfp-iframe" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+				$markup +='</div>';
+				$markup +='<div class="mfp-bottom-bar">';
+				$markup +='<div class="mfp-title"></div>';
+				
+				if( $counter ){
+					$markup +='<div class="mfp-counter"></div>';
+				}
+				
+				$markup +='</div>';
+			
+			var $iframe = {
+				markup: $markup ,
+					patterns: {
+						youtube: {
+							index: 'youtu', 
+							id: function(url) {   
+								 
+								var m = url.match( /^.*(?:youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/ );
+								if ( !m || !m[1] ) return null;
+								
+									if(url.indexOf('t=') != - 1){
+										
+										var $split = url.split('t=');
+										var hms = $split[1].replace('h',':').replace('m',':').replace('s','');
+										var a = hms.split(':');
+										
+										if (a.length == 1){
+											
+											$start = a[0]; 
+										
+										} else if (a.length == 2){
+											
+											$start = (+a[0]) * 60 + (+a[1]); 
+											
+										} else if (a.length == 3){
+											
+											$start = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
+											
+										}
+									}									
+									
+									var suffix = '?autoplay=1';
+									
+									if( $start > 0 ){
+										
+										suffix = '?start=' + $start + '&autoplay=1';
+									}
+								
+								return m[1] + suffix;
+							},
+							src: 'https://www.youtube.com/embed/%id%'
+						},
+						vimeo: {
+							index: 'vimeo.com/', 
+							id: function(url) {        
+								var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+								if ( !m || !m[5] ) return null;
+									
+								var suffix;
+								if(url.indexOf('#t=') != - 1){
+									var $split = url.split('#t=');
+									suffix = '#t='+$split[1].replace('?api=1&autoplay=1','');
+								}	
+																
+								var id = m[5] + '?autoplay=1'+ suffix;
+								return id;
+							},
+							src: 'https://player.vimeo.com/video/%id%'
+						}
+					}
+                };
+				
+			return $iframe;		
+			
+		},
 
         yjsgMedia: function () {
 
