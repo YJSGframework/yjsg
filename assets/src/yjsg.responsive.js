86e65e50b1d53837546151db04a3627c2b745c05
danyj <nocniagenti@gmail.com>
2019-03-06 14:47:37 +0100

    refactoring

--- assets/src/yjsg.responsive.js
+++ assets/src/yjsg.responsive.js
@@ -5,70 +5,108 @@
  * @license      http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
  * @websites     http://www.youjoomla.com | http://www.yjsimplegrid.com
  */
-(function ($) {
-    var YjsgRespond = {
-        settings: {
+;
+(function($, window, document, undefined) {
+
+    "use strict";
+
+    var pluginName = "YjsgRespond",
+        defaults = {
             menuholder: "#mmenu_holder",
             topmenuholder: "#topmenu_holder",
             selectmenu: "#mmenu"
-        },
-        initialize: function (options) {
-            this.options = $.extend({}, this.settings, options);
-            YjsgRespond.start();
-        },
-        start: function () {
+        };
+
+    function Plugin(element, options) {
+        this.element = element;
+
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+
+    $.extend(Plugin.prototype, {
+        init: function() {
+
             var self = this;
-            this.winsize = [];
-            YjsgRespond.showMenu();
-            YjsgRespond.menuFlip();
+            this.windowWidth = self.getWindowWidth();
+            self.showMenu();
+            self.menuFlip();
+
         },
-        showMenu: function () {
+
+        onResize: function() {
+            var self = this;
+            self.windowWidth = self.getWindowWidth();
+        },
+		
+        getWindowWidth: function() {
+			
+			var self = this;
+			var width = window.innerWidth
+			|| document.documentElement.clientWidth
+			|| document.body.clientWidth;
+			
+			return width;
+        },
+		
+        showMenu: function() {
             var self = this;
             if ($(self.settings.menuholder).length > 0) {
                 var select_holder = $(self.settings.menuholder);
                 $(self.settings.topmenuholder).prepend(select_holder);
-                $(self.settings.selectmenu).on('change', function () {
+                $(self.settings.selectmenu).on('change', function() {
                     window.location.href = $(this).val();
                 });
             }
         },
-        menuFlip: function () {
+		
+        menuFlip: function() {
             var self = this;
-            self.winsize["width"] = YjsgRespond.viewPort().width;
-            $("ul.yjsgmenu li.haschild").hover(function () {
+            self.windowWidth = self.getWindowWidth();
+            $("ul.yjsgmenu li.haschild").hover(function() {
                 $(this).removeClass("flip");
                 var findMargin = $('ul.yjsgmenu ul ul').first().css('margin-left');
                 var dropWidth = $(this).find('div.ulholder').first().outerWidth(true) + parseInt(findMargin); // ul side push 10px
-                ltrOffset = $(this).find('div.ulholder').first().offset().left + dropWidth;
-                rtlOffset = self.winsize["width"] - $(this).find('div.ulholder').first().offset().left;
+                var ltrOffset = $(this).find('div.ulholder').first().offset().left + dropWidth;
+                var rtlOffset = self.windowWidth - $(this).find('div.ulholder').first().offset().left;
                 var offset = ltrOffset;
                 if ($("body").hasClass("yjsgrtl")) {
                     offset = rtlOffset;
                 }
-                if (offset >= self.winsize["width"]) {
+                if (offset >= self.windowWidth) {
                     $(this).addClass("flip");
                 }
-            }, function () {
-                setTimeout(function () {
+            }, function() {
+                setTimeout(function() {
                     $(this).removeClass("flip");
                 }, 300);
             });
-        },
-        viewPort: function () {
-            var e = window,
-                a = 'inner';
-            if (!('innerWidth' in window)) {
-                a = 'client';
-                e = document.documentElement || document.body;
-            }
-            return {
-                width: e[a + 'Width'],
-                height: e[a + 'Height']
-            };
         }
-    }
-    $(document).on('ready', YjsgRespond.initialize);
-    $(window).on('resize', function () {
-        YjsgRespond.winsize["width"] = YjsgRespond.viewPort().width;
     });
-})(jQuery);
+
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            } else if (Plugin.prototype[options]) {
+                $.data(this, 'plugin_' + pluginName)[options]();
+            }
+        });
+    }
+
+})(jQuery, window, document);
+
+
+(function($) {
+    $(document).on('ready', function() {
+        $(document).YjsgRespond();
+    });
+
+    $(window).on('resize', function() {
+        $(document).YjsgRespond('onResize');
+    });
+
+}(jQuery));
